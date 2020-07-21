0a910d9a87c09cbd23b0e1d4199527134c1fa48a
Paul Saxe <paul@thesaxes.net>
2020-07-21 16:13:05 -0400

    Initial, simple tests just to get going.

--- tests/test_psi4_step.py
+++ tests/test_psi4_step.py
@@ -3,5 +3,45 @@
 
 """Tests for `psi4_step` package."""
 
-import pytest  # noqa: F401
-import psi4_step  # noqa: F401
+import pytest
+import psi4_step
+import re  # noqa: F401
+
+
+@pytest.fixture
+def instance():
+    instance = psi4_step.Psi4()
+    instance._id = (1,)
+    return instance
+
+
+def test_construction():
+    """Simplest test that we can make a Psi4 object"""
+    instance = psi4_step.Psi4()
+    assert (str(type(instance)) == "<class 'psi4_step.psi4.Psi4'>")
+
+
+def test_version():
+    """Test that the object returns a version"""
+    instance = psi4_step.Psi4()
+    result = instance.version
+    assert isinstance(result, str) and len(result) > 0
+
+
+def test_git_revision():
+    """Test that the object returns a git revision"""
+    instance = psi4_step.Psi4()
+    result = instance.git_revision
+    assert isinstance(result, str) and len(result) > 0
+
+
+# def test_description_text_default(instance):
+#     """Test the default description text"""
+
+#     print(instance.description_text())
+#     assert re.fullmatch(
+#         (
+#             r'Step 1: Psi4  [-+.0-9a-z]+\n'
+#             r'    Create a 2 x 2 x 2 supercell from the current cell'
+#         ), instance.description_text()
+#     ) is not None
