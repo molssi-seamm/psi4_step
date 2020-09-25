93f86213a0e13222b74a80ce4a046a634c75f6c6
Paul Saxe <paul@thesaxes.net>
2020-09-25 09:28:36 -0400

    Updating to python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -74,8 +74,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         'org.molssi.seamm': [
