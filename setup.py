6f248d36c5d72d8cd0d75515e1a0792878e8fc31
Paul Saxe <paul@thesaxes.net>
2021-02-11 17:53:08 -0500

    Updated the short description and keywords to better represent the plug-in.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-psi4_step
-A step in SEAMM to setup, run and analyze quantum chemistry calculations
-using Psi4
+"""psi4_step
+A SEAMM plug-in to setup, run and analyze quantum chemistry calculations using Psi4
 """
 import sys
 from setuptools import setup, find_packages
 import versioneer
 
-short_description = __doc__.split("\n")
+short_description = __doc__.splitlines()[1]
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
@@ -29,45 +27,28 @@ setup(
     name='psi4_step',
     author="Paul Saxe",
     author_email='psaxe@molssi.org',
-    description=short_description[1],
+    description=short_description,
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license="BSD-3-Clause",
     url='https://github.com/molssi-seamm/psi4_step',
-
-    # Which Python importable modules should be included when your package is
-    # installed, handled automatically by setuptools. Use 'exclude' to prevent
-    # some specific subpackage(s) from being added, if needed
     packages=find_packages(include=['psi4_step']),
-
-    # Optional include package data to ship with your package. Customize
-    # MANIFEST.in if the general case does not suit your needs. Comment out
-    # this line to prevent the files from being packaged with your software
     include_package_data=True,
-
-    # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-
-    # Required packages, pulls from pip if needed; do not use for Conda
-    # deployment
     install_requires=requirements,
-
     test_suite='tests',
-
-    # Valid platforms your code works on, adjust to your flavor
     platforms=['Linux',
                'Mac OS-X',
                'Unix',
                'Windows'],
+    zip_safe=True,
 
-    # Manual control if final package is compressible or not, set False to
-    # prevent the .egg from being made
-    zip_safe=False,
-
-    keywords='psi4_step',
+    keywords=['SEAMM', 'plug-in', 'flowchart', 'Psi4', 'DFT',
+              'quantum chemistry', 'CCSD'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics',
